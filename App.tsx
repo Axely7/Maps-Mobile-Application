1b6dd301e13421cafcb8ab645558fe0258c7f33f
Axel Jimenez <axely7@hotmail.com>
2023-05-19 21:04:26 -0600

    Init, installing React Native Vector Icons

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return (
+    <>
+      <Icon name="star-outline" color="red" size={50} />
+    </>
+  );
+};
+
+export default App;
